0b153046799580fb83417e59efb4f7b3301a8baa
Tram <ntramvl@gmail.com>
2023-02-03 13:31:49 +0700

    setup Action text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get '/info' => 'pages#info', as: :info
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get info" do
+    get pages_info_url
+    assert_response :success
+  end
+end
